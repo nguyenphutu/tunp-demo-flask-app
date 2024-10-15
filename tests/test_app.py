f21744126c185c57fccdbad468e9a7a8a39fffc8
Nguyễn Phú Tú <tunp.dbn@vnpt.vn>
2024-10-15 15:50:55 +0700

    update

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,18 @@
 import pytest
-from app import app
+from app.app import app
+
 
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
 
+
 def test_home(client):
     rv = client.get('/')
     json_data = rv.get_json()
     assert json_data['message'] == 'Hello, CI/CD with Jenkins and Docker!'
+
 
 def test_status(client):
     rv = client.get('/status')
